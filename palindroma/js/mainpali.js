cf90b796dbe4c617a0186cd2e2892273f6c4b184
chiara.ruggi <chiara.ruggi1998@gmail.com>
2023-11-07 10:16:08 +0100

    reverse word

---
+++ palindroma/js/mainpali.js
@@ -0,0 +1,14 @@
+let userWord = document.getElementById("userInput");
+
+
+
+  
+function reverse(){
+    let userString = userWord.value;
+    userString= userString.split('');
+    userString= userString.reverse();
+}
+
+document.getElementById("confirmButton").addEventListener("click", function(){
+
+});
